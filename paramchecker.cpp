eac5c57de2ae907c710d59345c05a22a97c0cc5d
mygithubaccountname <56979805+mygithubaccountname@users.noreply.github.com>
2020-07-20 15:37:19 +0530

    Updated paramchecker.cpp
    Modularized the checks to adhere to SRP

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,11 +1,35 @@
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
   //int i;
+  bool bloodPressureStatus, oxygenationStatus, respiratoryStatus;
+  
+  bloodPressureStatus = bloodPressureIsOk(bpm);
+  oxygenationStatus = oxygenationIsOk(spo2);
+  respiratoryStatus = respirationIsOk(respRate);
+  
+  if (!bloodPressureStatus || !oxygenationStatus || !respiratoryStatus) {
+  return false;
+  }
+  else {
+  return true; }
+}
+
+bool bloodPressureIsOk(float bpm) {
   if(bpm < 70 || bpm > 150) {
     return false;
-  } else if(spo2 < 80) {
+  }
+  return true;
+}
+
+bool oxygenationIsOk(float spo2) {
+  if(spo2 < 80) {
     return false;
-  } else if(respRate < 30 || respRate > 60) {
+  }
+  return true;
+}
+
+bool respirationIsOk(float respRate) {
+  if(respRate < 30 || respRate > 60) {
     return false;
   }
   return true;
