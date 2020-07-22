cd08dd1c511bb42e00903f4f2ee3e7a597e7a8c5
mygithubaccountname <56979805+mygithubaccountname@users.noreply.github.com>
2020-07-22 14:51:26 +0530

    Update paramchecker.cpp

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -31,31 +31,25 @@ class parameter{
 };
 
 
-void initializeLimits(parameter*);
 void setReading(parameter*, vitals*);
+void setLimits(parameter*);
 bool checkVitals(parameter*);
 
 bool vitalsAreOk(vitals* vitalReadings) {
    bool testResult;
-   parameter parameterList[3];
+   parameter parameterList[10];
    
-   initializeLimits(parameterList);
    setReading(parameterList, vitalReadings);
+   setLimits(parameterList);
    testResult = checkVitals(parameterList);    
    
    return testResult;
 }
 
-void initializeLimits(parameter* list){
-  list[0].setLimits(70, 150);
-  list[1].setLimits(80, 100);
-  list[2].setLimits(30, 60);
-}
-
 void setReading(parameter* list, vitals* vitalReadings){
-  list[0].setValue(vitalReadings->bpm);
-  list[1].setValue(vitalReadings->spo2);
-  list[2].setValue(vitalReadings->respRate);
+   for (int loopVariable = 0; loopVariable < vitalReading.numberOfParmeters; loopVariable++) {
+      list[loopVariable].setValue(vitalReadings->vital[loopVariable]);
+   }
 }
 
 bool checkVitals(parameter* list){
@@ -66,4 +60,10 @@ bool checkVitals(parameter* list){
   }
 
    return vitalStatus;
+}
+
+void setLimits(parameter* list){
+  list[0].setLimits(70, 150);
+  list[1].setLimits(80, 100);
+  list[2].setLimits(30, 60);
 }
