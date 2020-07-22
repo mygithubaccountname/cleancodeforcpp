c7a93a7f0482c1d9fe71db9a06063337b142c3d6
mygithubaccountname <56979805+mygithubaccountname@users.noreply.github.com>
2020-07-22 15:50:45 +0530

    Update paramchecker.cpp

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -7,7 +7,7 @@ class reading{
       float limitRange[10][2] = {70,50,80,100,30,60};
 
    public:
-      reading(): limitRange[0][0](70),  limitRange[0][1](50),  limitRange[1][0](80),  limitRange[1][1](100),  limitRange[2][0](30),  limitRange[2][1](60) { }
+      reading(): { }
       bool isReadingWithinLimits(float reading, int index){
       if ((limitRange[index][0] <= reading) && (reading <= limitRange[index][1]))
           return true;
