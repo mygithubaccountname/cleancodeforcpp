2c5cf0084c7306fc17fad49d127dbff6c8ffecfe
mygithubaccountname <56979805+mygithubaccountname@users.noreply.github.com>
2020-07-22 12:53:34 +0530

    Update testparamchecker.cpp

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -4,14 +4,14 @@
 TEST(VitalsTest, BPM) { 
     vitalReading.bpm = 100;
     vitalReading.spo2 = 100;
-    vitalReading.resp = 50;
+    vitalReading.respRate = 50;
     ASSERT_EQ(true, vitalsAreOk(&vitalReading));
 }
  
 TEST(VitalsTest, SPO2) {
      vitalReading.bpm = 100;
     vitalReading.spo2 = 40;
-    vitalReading.resp = 50;
+    vitalReading.respRate = 50;
     ASSERT_EQ(false, vitalsAreOk(&vitalReading));
 }
  
