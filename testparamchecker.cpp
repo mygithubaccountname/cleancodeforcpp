d3badda874b26195084998022566b81f41b44891
mygithubaccountname <56979805+mygithubaccountname@users.noreply.github.com>
2020-07-22 14:47:57 +0530

    Update testparamchecker.cpp

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -6,6 +6,7 @@ TEST(VitalsTest, BPM) {
     vitalReading.vital[0] = 100; //bpm
     vitalReading.vital[1] = 100; //spo2
     vitalReading.vital[2] = 50; //respRate
+    vitalReading.numberOfParmeters = 3;
     ASSERT_EQ(true, vitalsAreOk(&vitalReading));
 }
  
@@ -13,6 +14,7 @@ TEST(VitalsTest, SPO2) {
     vitalReading.vital[0] = 100; //bpm
     vitalReading.vital[1] = 40; //spo2
     vitalReading.vital[2] = 50; //respRate
+    vitalReading.numberOfParmeters = 3;
     ASSERT_EQ(false, vitalsAreOk(&vitalReading));
 }
  
